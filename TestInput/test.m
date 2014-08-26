8ebf5253f6bf0d2dd34795aec70f46bc83eb3920
Chris Harding <chris@motrr.com>
2014-08-26 17:30:49 +0800

    Added test input for other NSLocalizedString macros.

--- TestInput/test.m
+++ TestInput/test.m
@@ -3,3 +3,7 @@ NSLocalizedStringWithDefaultValue(@"button_ok", @"StandardInterface", [NSBundle 
 NSLocalizedStringWithDefaultValue(@"key1", @"Localizable", [NSBundle mainBundle], @"Ok", @"Button to confirm a choice");
 
 NSLocalizedStringWithDefaultValue(@"multiline", @"Localizable", [NSBundle mainBundle], @"Two lines already localized", @"Some Multiline Comment")
+
+NSLocalizedString(@"macro_key1", @"Shortcut macro for main bundle and default .strings table file")
+
+NSLocalizedStringFromTable(@"macro_key2", @"Localizable",  @"Shortcut macro for main bundle name .strings table file")
